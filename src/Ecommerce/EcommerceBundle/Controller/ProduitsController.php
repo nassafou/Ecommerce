77f715b0507b383ca28a504ebd4f90764c02bd34
nassafou <ntyoussouf@gmail.com>
2016-01-31 08:32:53 -0800

    modif

--- src/Ecommerce/EcommerceBundle/Controller/ProduitsController.php
+++ src/Ecommerce/EcommerceBundle/Controller/ProduitsController.php
@@ -3,12 +3,8 @@
 namespace Ecommerce\EcommerceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-
 use Ecommerce\EcommerceBundle\Entity\Categories;
-
 use Ecommerce\EcommerceBundle\Controller\PanierController;
-
 use Ecommerce\EcommerceBundle\Form\RechercheType;
 use Ecommerce\EcommerceBundle\Twig\Extension;
 
@@ -17,37 +13,20 @@ class ProduitsController extends Controller
 {
     public function produitsAction(Categories $categorie = null)
     {
-        
         //var_dump($categorie);
-       // die();
-        
-        
+       // die();   
         $session = $this->getRequest()->getSession();
-        
         // création de l'entity manager
         $em = $this->getDoctrine()->getManager();
-        
-        
-        
-        
-        
-        
         if($categorie != null)
-        $produits = $em->getRepository('EcommerceBundle:Produits')->byCategorie($categorie);
-            
+        $produits = $em->getRepository('EcommerceBundle:Produits')->byCategorie($categorie);   
         else
         // création du repository 
         $produits = $em->getRepository('EcommerceBundle:Produits')->findby(array());
-        
-        
-        
         if($session->has('panier'))
        $panier = $session->get('panier');
         else 
             $panier = false;
-        
-        
-        
         return $this->render('EcommerceBundle:Default:produits/layout/produits.html.twig', array('produits' => $produits,
                                                                                                  'panier' => $panier ));
     }
@@ -58,10 +37,7 @@ class ProduitsController extends Controller
         $session = $this->getRequest()->getSession();
         // créer l'entitie manager
         $em = $this->getDoctrine()->getManager();
-        
         $produit = $em->getRepository('EcommerceBundle:Produits')->find($id);
-        
-        
         if(!$produit){ throw $this->createNotFoundException('La page n\'existe pas ');}
         
         if($session->has('panier'))
@@ -78,21 +54,13 @@ class ProduitsController extends Controller
     public function categorieAction($categorie)
     {
         // query bulder en fonction de la catégorie afficher les produits
-        
         //créer le entitie manager
-        
         $em = $this->getDoctrine()->getManager();
-        
         // on va dans le repository
         $produits = $em->getRepository('EcommerceBundle:Produits')->byCategorie($categorie);
-        
         $categorie = $em->getRepository('EcommerceBundle:Categories')->find($categorie);
-        
         //condition
-        
         if(!$categorie){ throw $this->createNotFoundException('la page n\'existe pas '); }
-        
-        
         return $this->render('EcommerceBundle:Default:produits/layout/categorieProduits.html.twig', array('produits' => $produits));
     }
     
@@ -101,34 +69,22 @@ class ProduitsController extends Controller
     public function rechercheAction()
     {
         $form = $this->createForm( new RechercheType()); 
-        
-        return $this->render('EcommerceBundle:Default:recherche/modulesUsed/recherche.html.twig', array('form' => $form->createView() ));
-        
+        return $this->render('EcommerceBundle:Default:recherche/modulesUsed/recherche.html.twig', array('form' => $form->createView() ));   
     }
         
     public function rechercheTraitementAction()
     {
-        
      $form = $this->createForm( new RechercheType());    
         if( $this->get('request')->getMethod() == 'POST')
         {
             $form->bind($this->get('request'));
             echo $form['recherche']->getData();
-            
-        
-        
-        
         //die();
        $em = $this->getDoctrine()->getManager();
-       
        $produits = $em->getRepository('EcommerceBundle:Produits')->recherche($form['recherche']->getData());
         }else{
             throw $this->createNotFoundException('La page n\'exist' );
         }
-       
-       
-       
-       
        return $this->render('EcommerceBundle:Default:produits/layout/produits.html.twig', array('produits' => $produits));
        
     }
